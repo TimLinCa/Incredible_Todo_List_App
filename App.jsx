ec40b72d829faa5bcb61d68b2b0400b165474fef
TimLinCa <TimLin626@gmail.com>
2024-01-26 17:13:47 -0700

    Lab2

---
+++ App.jsx
@@ -0,0 +1,24 @@
+/**
+ * My To Do List App
+ *
+ * @format
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+} from 'react-native';
+import ToDoList from './components/ToDoList';
+import ToDoForm from './components/ToDoForm';
+
+function App() {
+  return (
+    <SafeAreaView>
+      <ToDoList></ToDoList>
+      <ToDoForm></ToDoForm>
+    </SafeAreaView>
+  );
+}
+
+
+export default App;
